c2ac433e6a87157b88560c6bd5f3c2ca197bec94
CherleX <756907672@qq.com>
2016-08-11 20:29:24 +0800

    [137. Single Number II][Accepted]committed by LeetCode Extension

--- 137-Single-Number-II/solution.py
+++ 137-Single-Number-II/solution.py
@@ -4,4 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        return int((sum(set(nums))*3-sum(nums))/2)
+        ans = 0
+        for i in xrange(0,32):
+            count = 0
+            for a in nums:
+                if ((a >> i) & 1):
+                    count+=1
+            ans |= ((count%3) << i)
+        return self.convert(ans)
+        
+    def convert(self,x):
+        if x >= 2**31:
+            x -= 2**32
+        return x
