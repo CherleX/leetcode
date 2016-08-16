71c721833689d605b993c0893a117195ad2863c2
CherleX <756907672@qq.com>
2016-08-16 19:39:57 +0800

    [367. Valid Perfect Square][Accepted]committed by LeetCode Extension

---
+++ 367-Valid-Perfect-Square/solution.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        left, right = 0, num
+        while left <= right:
+            mid = int((left + right) / 2)
+            if mid ** 2 > num:
+                right = mid - 1
+            elif mid ** 2 == num:
+                return True
+            else:
+                left = mid + 1
+        return False
