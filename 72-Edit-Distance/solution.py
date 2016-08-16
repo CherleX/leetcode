f9d239bac988f38fc3d9ca917939fdc8570bac8c
CherleX <756907672@qq.com>
2016-08-16 17:23:42 +0800

    [72. Edit Distance][Accepted]committed by LeetCode Extension

---
+++ 72-Edit-Distance/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def minDistance(self, word1, word2):
+        # initialization
+        len1, len2 = len(word1), len(word2)
+        dp = [[0] * (len2 + 1) for i in xrange(len1 + 1)]
+        for i in xrange(1, len1 + 1): dp[i][0] = i
+        for j in xrange(1, len2 + 1): dp[0][j] = j
+        # dp
+        for i in xrange(1, len1 + 1):
+            for j in xrange(1, len2 + 1):
+                dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, 
+                    dp[i - 1][j - 1] if word1[i - 1] == word2[j - 1] else dp[i - 1][j - 1] + 1)
+        return dp[len1][len2]
