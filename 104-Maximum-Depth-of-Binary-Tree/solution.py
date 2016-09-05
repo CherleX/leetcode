450c5fe44c571c9f297979f94a9fb4dcdd8a54a4
CherleX <756907672@qq.com>
2016-09-05 20:00:14 +0800

    [104. Maximum Depth of Binary Tree][Accepted]committed by LeetCode Extension

--- 104-Maximum-Depth-of-Binary-Tree/solution.py
+++ 104-Maximum-Depth-of-Binary-Tree/solution.py
@@ -4,15 +4,6 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        depth = 0
-        level = [root] if root else []
-        while level:
-			depth += 1
-			queue = []
-			for lev in level:
-				if lev.left:
-					queue.append(lev.left)
-				if lev.right:
-					queue.append(lev.right)
-			level = queue
-        return depth
+        if root == None:
+            return 0
+        return 1 + max(self.maxDepth(root.left),self.maxDepth(root.right))
