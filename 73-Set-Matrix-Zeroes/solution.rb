d3cbed19d4fc02484cdf19b8bf300e02a3b36963
CherleX <756907672@qq.com>
2016-09-30 16:08:19 +0800

    [73. Set Matrix Zeroes][Accepted]committed by LeetCode Extension

---
+++ 73-Set-Matrix-Zeroes/solution.rb
@@ -0,0 +1,26 @@
+def set_zeroes(matrix)
+  return if matrix.empty?
+
+  m, n = matrix.size, matrix[0].size
+  row0marker = (0...n).any? { |j| matrix[0][j].zero? }
+  col0marker = (0...m).any? { |i| matrix[i][0].zero? }
+
+  (1...m).each do |i|
+    (1...n).each do |j|
+      if matrix[i][j].zero?
+        matrix[0][j] = matrix[i][0] = 0
+      end
+    end
+  end
+
+  (1...m).each do |i|
+    (1...n).each do |j|
+      if matrix[0][j].zero? || matrix[i][0].zero?
+        matrix[i][j] = 0
+      end
+    end
+  end
+
+  ((0...n).each { |j| matrix[0][j] = 0 }; nil) if row0marker
+  ((0...m).each { |i| matrix[i][0] = 0 }; nil) if col0marker
+end
